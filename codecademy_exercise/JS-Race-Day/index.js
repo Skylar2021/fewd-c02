225dc73a144f4875f689201b8c8038c5161470a6
Skylar2021 <maplewong5@yahoo.com.hk>
2021-07-21 16:37:23 +0800

    finish race day

---
+++ codecademy_exercise/JS-Race-Day/index.js
@@ -0,0 +1,25 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+
+let earlyRunner = true;
+let runnerAge = 19;
+// checks whether the runner is an adult AND registered early
+if (earlyRunner && runnerAge > 18) {
+  raceNumber += 1000;
+  console.log(`For adult runner who registered early, your race number is ${raceNumber}.`)
+} else if (!earlyRunner && runnerAge > 18) {
+  console.log(`Runner is not an adult and registered early and your race number is ${raceNumber}.`)
+}
+
+//check age and registration time to determine race time
+if (earlyRunner && runnerAge > 18) {  
+  console.log(`For runners over 18 who registered early, they will race at 9:30 am and ${raceNumber} is your race number.`)
+} else if (!earlyRunner && runnerAge > 18){
+  console.log(`For adult runner who did not register early they will race at 11:00am and ${raceNumber} is your race number.`)
+} else if (earlyRunner && runnerAge < 18) {
+  console.log(`Youth runners who registered early,race at 12:30 pm and ${raceNumber} is your race number.`)
+} else if (!earlyRunner && runnerAge < 18) {
+  console.log(`Youth runners who did not register early,race at 12:30 pm and ${raceNumber} is your race number.`)
+} else {
+  console.log('For runner who is exactly 18 years old, please approach the registration desk.')
+}
