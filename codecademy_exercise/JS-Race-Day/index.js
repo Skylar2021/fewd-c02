89d018b9d94102c6ebf41b0d0813f1a0d909b220
Skylar2021 <maplewong5@yahoo.com.hk>
2021-07-22 21:30:43 +0800

    teacher demo

--- codecademy_exercise/JS-Race-Day/index.js
+++ codecademy_exercise/JS-Race-Day/index.js
@@ -1,15 +1,12 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
 let earlyRunner = true;
-let runnerAge = 19;
+let runnerAge = 18;
 
-// checks whether the runner is an adult AND registered early and their race number
+// checks whether the runner is an adult AND registered early, log their race number
 if (earlyRunner && runnerAge > 18) {
   raceNumber += 1000;
-  console.log(`For adult runner who registered early, your race number is ${raceNumber}.`)
-} else if (!earlyRunner && runnerAge > 18) {
-  console.log(`Runner is not an adult and registered early and your race number is ${raceNumber}.`)
-}
+} 
 
 //check age and registration time to determine race time and race number
 if (earlyRunner && runnerAge > 18) {  
@@ -23,3 +20,18 @@ if (earlyRunner && runnerAge > 18) {
 } else {
   console.log('For runner who is exactly 18 years old, please approach the registration desk.')
 }
+
+/* 
+// switch-case approach
+switch ( (age < 18) ? 'child': 'adult' ) {
+  case 'child':
+    console.log(`${raceNumber}: Race start at 12:30am`)
+  break;
+  case 'adult':
+    earlyRegister ? 
+    (age > 18) && console.log(`${raceNumber}: Race start at 9:30am`) :
+    (age > 18) && console.log(`${raceNumber}: Race start at 11:00am`);
+    (age == 18) && console.log(`Please go to registration desk`)
+  break;
+
+  */
