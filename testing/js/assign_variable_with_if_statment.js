e3b44c1e8e3c71424d64844b2e9c8ea797821c74
Skylar2021 <maplewong5@yahoo.com.hk>
2021-07-30 21:27:16 +0800

    renamed leetcode

---
+++ testing/js/assign_variable_with_if_statment.js
@@ -0,0 +1,6 @@
+function num(n) {
+  let i = (n > 0) ? 'positive' : 'negative';
+  console.log(i)
+}
+
+num(-10)
--- testing/js/test.js
+++ testing/js/test.js
@@ -1,6 +1,47 @@
-function num(n) {
-  let i = (n > 0) ? 'positive' : 'negative';
-  console.log(i)
-}
 
-num(10)
+
+const person = {
+  firstName: 'Wong',
+  lastName: 'Skylar',
+}
+const { firstName, lastName } = person
+console.log('Name : ' + lastName) // Name : Skylar
+console.log('Last Name : ' + firstName) // Last Name : Skylar
+
+let arr = Object.valueOf(person)
+console.log(arr)
+
+
+
+odj = { 
+  _name: 'Cherie',
+  get name() { 
+  return this._name
+  }, 
+  set rename(input) {  // setter receive a parameter
+    if (typeof input === 'string'){
+      this._name = input 
+    } else {
+      console.log('Please enter a string!')
+    }
+  }
+} 
+console.log('get ' + odj.name) // to call getter/setter, no () is required 
+
+console.log(odj.name) // Cherie
+odj.rename = 'Skylar'
+console.log(odj.name) // Skylar
+
+//Factory Function
+const odjFactory = (key1, key2) => {
+  return {
+    key1,
+    key2,
+    log() {
+      console.log('This is how Factory Function is working.')
+    }
+  }
+}
+const firstTry = odjFactory('value1', 'value2')
+console.log(firstTry) // log: { key1: 'value1', key2: 'value2', log: [Function: log] }
+firstTry.log() // log: This is how Factory Function is working.
