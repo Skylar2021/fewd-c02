e0b84e050e9941a77393326cd85f68122bb083a6
Skylar2021 <maplewong5@yahoo.com.hk>
2021-08-03 14:27:39 +0800

    leetcode 13 undone

---
+++ Leetcode/13_Roman_to_integer.js
@@ -0,0 +1,27 @@
+
+
+
+// if (s.includes('I') && s.includes('V') || s.includes('X') ) {
+//   if ( s.indexOf('I') < s.indexOf(''))
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {
+   if (s.length === 0) return;
+   if (s.length > 15 ) return;
+   const num = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+  for (i = 0; i < s.length; i++) {
+
+  }
+
+   
+};
