dd8be142292c9bc904da0107c15b17409b955321
Skylar2021 <maplewong5@yahoo.com.hk>
2021-08-14 18:37:22 +0800

    added .catch()

--- testing/js/async_await.js
+++ testing/js/async_await.js
@@ -1,18 +1,16 @@
 const answer = require('./promise_apply.js')
 
 
-const thinking = () => {
-  let arr = new Array(20).fill('um...')
-  for (let i of arr){
-    console.log(i)
-  }
-}
+
 
 async function said() {
   console.log("I xyz you! Let's be couple!")
-  thinking()
+  answer.thinking()
   let reply = await answer.speakUp()
   console.log(`My reply is ... ${reply}`)
 }
 
-said()
+said().catch((reject) => {
+  console.log(reject)
+  
+})
